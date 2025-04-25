0843eabea4e4f6021606438675fb4862fc057b3c
amabe418 <valienteamalia418@gmail.com>
2025-04-25 19:18:43 -0400

    temporal

--- bot.py
+++ bot.py
@@ -1,9 +1,5 @@
 # hupper -m bot.py
-import os
-import threading
-from flask import Flask
 from telegram import Update
-from google.oauth2.service_account import Credentials
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # region Metodos
@@ -16,24 +12,14 @@ async def horario(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # endregion
 
 # region Principal
-app = Flask(__name__)
 
 token = "7906733724:AAHvuyxL5t9lr5_NZpnphckAeL1Zj3Ogw10"
 application = ApplicationBuilder().token(token).build()
 application.add_handler( CommandHandler("start", welcome ))
 application.add_handler(CommandHandler("horario", horario))
-# application.run_polling(allowed_updates=Update.ALL_TYPES)
-
-@app.route('/')
-def home():
-    return "El bot de Telegram está funcionando."
-
-def start_telegram_bot():
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-
-thread = threading.Thread(target=start_telegram_bot)
-thread.start()
+application.run_polling(allowed_updates=Update.ALL_TYPES)
 # endregion
 
+# region Despliegue
 
 #endregion
