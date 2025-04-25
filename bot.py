c87ef0d422ac5fd8c9ad069619c83335cc954651
amabe418 <valienteamalia418@gmail.com>
2025-04-25 14:43:15 -0400

    requerimientos para el despliegue

--- bot.py
+++ bot.py
@@ -1,14 +1,27 @@
+# hupper -m bot.py
+import gspread
+from google.oauth2.service_account import Credentials
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import hupper
 
+# region Metodos
 async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("¡Hola, te doy la bienvenida al bot de deportes de la Universidad de la Habana!")
 
 async def horario(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Los horarios aún no están disponibles, pero pronto lo estarán.")
 
-application = ApplicationBuilder().token("7906733724:AAHvuyxL5t9lr5_NZpnphckAeL1Zj3Ogw10").build()
+# endregion
+
+# region Principal
+
+token = "7906733724:AAHvuyxL5t9lr5_NZpnphckAeL1Zj3Ogw10"
+application = ApplicationBuilder().token(token).build()
 application.add_handler( CommandHandler("start", welcome ))
 application.add_handler(CommandHandler("horario", horario))
 application.run_polling(allowed_updates=Update.ALL_TYPES)
+# endregion
+
+# region Despliegue
+
+#endregion
