faf4ee50df99deae981f7313343c78a4888f28b1
JabelResendiz <jabelresendiz18@gmail.com>
2025-06-21 17:12:45 -0400

    final

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
-# Construir la imagen
-echo "Construyendo la imagen 'bot-telegram'..."
-docker build -t bot-telegram .
 
-# Ejecutar el contenedor
-echo "Ejecutando el contenedor..."
-docker run -d --name bot-telegram-container bot-telegram
+PROJECT_DIR="./src"
+
+if [ ! -d "$PROJECT_DIR" ]; then
+  echo "❌ Error: La carpeta 'src' no existe en el directorio actual."
+  exit 1
+fi
+
+cd "$PROJECT_DIR" || exit 1
+
+
+echo "Instalando dependencias..."
+python -m pip install -r requirements.txt  
+
+
+echo "Iniciando el bot de Telegram..."
+python main.py  
